82bdb7211313a52f02fc935f80f92fabe04a074e
Admin <Admin@192.168.1.90>
2014-12-13 12:32:12 +0100

    asd

---
+++ rvsguenther/src/rvsguenther/main.java
@@ -0,0 +1,14 @@
+package rvsguenther;
+
+import chatserver.ChatServer;
+
+public class main {
+	
+	ChatServer a = new ChatServer();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
